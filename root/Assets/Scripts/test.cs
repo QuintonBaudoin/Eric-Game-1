47383a9c9aa0760e225d8a4a8dbe99fd90cd7b83
Zac-King <zacharyking18@yahoo.com>
2015-05-14 17:09:28 -0500

    initial commit
    commit to the remote

---
+++ root/Assets/Scripts/test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel(theLevel);
+        }
+    }
+
+    public string theLevel;
+}
